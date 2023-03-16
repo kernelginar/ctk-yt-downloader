3350c387fb92679a8022b11bbdaf061f5c2e84f4
Abdullah <80760611+kernelginar@users.noreply.github.com>
2023-03-16 13:38:02 +0300

    Update and rename main.py to py-yt-downloader.py

--- py-yt-downloader.py
+++ py-yt-downloader.py
@@ -1,7 +1,6 @@
-import tkinter as tk
-from tkinter import filedialog
 import customtkinter as ctk
 from pytube import YouTube
+import os
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
@@ -9,6 +8,12 @@ ctk.set_default_color_theme("dark-blue")
 class YT_Downloader(ctk.CTk):
     def __init__(self):
         super().__init__()
+        
+        self.title("YouTube Video - Audio Downloader")
+        self.geometry("800x600")
+        self.minsize(800, 600)
+
+        ################################# Video ######################################
         
         # Download Function
         def startDownloadVideo():
@@ -22,12 +27,13 @@ class YT_Downloader(ctk.CTk):
                 video_ytObject = YouTube(video_link_yt, on_progress_callback=video_on_progress)
                 
                 video = video_ytObject.streams.filter(file_extension=video_file_extension_yt).get_highest_resolution()
-                video.download(filename=video_file_name_yt+"."+video_file_extension_yt)
+                video.download(output_path=os.getcwd(), filename=video_file_name_yt+"."+video_file_extension_yt)
                 video_finish_label.configure(text="Downloaded!")
             except:
                 video_finish_label.configure(text="Failed to download video!")
             video_finish_label.configure(text="Video downloaded!")
-                    
+            
+            
         # Video Progress Bar Function
         def video_on_progress(stream, chunk, bytes_remaining):
             total_size = stream.filesize
@@ -38,14 +44,7 @@ class YT_Downloader(ctk.CTk):
             video_pPercentage.update()
             
             video_progressBar.set(float(percentage_of_completion) / 100)
-        
-        self.title("YouTube Video - Audio Downloader")
-        self.geometry("800x600")
-        self.minsize(800, 600)
-        
-        
-        ################################# Video ######################################
-        
+                
         
         # Main Tab
         tab = ctk.CTkTabview(master=self)
@@ -66,9 +65,9 @@ class YT_Downloader(ctk.CTk):
         video_file_name.pack(padx=10, pady=10)
         
         # Video File Extension
-        video_file_extension = ctk.CTkEntry(master=tab.tab("Video"), width=350, height=40, placeholder_text="File Extension: mp4, mkv")
+        video_file_extension = ctk.CTkEntry(master=tab.tab("Video"), width=350, height=40, placeholder_text="File Extension: mp4, mkv etc...")
         video_file_extension.pack(padx=10, pady=10)
-        
+                
         # Video Finished label
         video_finish_label = ctk.CTkLabel(master=tab.tab("Video"), text="")
         video_finish_label.pack()
@@ -85,6 +84,9 @@ class YT_Downloader(ctk.CTk):
         video_download = ctk.CTkButton(master=tab.tab("Video"), text="Download", command=startDownloadVideo)
         video_download.pack(padx=10, pady=20)
         
+        # Video Current directory
+        video_current_directory_label = ctk.CTkLabel(master=tab.tab("Video"), text=f"Current Directory: {os.getcwd()}")
+        video_current_directory_label.pack(padx=10, pady=10)
         
         ############################## Audio #############################
         
@@ -100,7 +102,7 @@ class YT_Downloader(ctk.CTk):
                 
                 audio_ytObject = YouTube(audio_link_yt, on_progress_callback=on_progress)
                 audio = audio_ytObject.streams.get_audio_only()
-                audio.download(filename=audio_file_name_yt+"."+audio_file_extension_yt)
+                audio.download(output_path=os.getcwd(), filename=audio_file_name_yt+"."+audio_file_extension_yt)
             except:
                 audio_finish_label.configure(text="YouTube link is invalid")
             audio_finish_label.configure(text="Downloaded!")
@@ -148,7 +150,11 @@ class YT_Downloader(ctk.CTk):
                
         # Audio Download Button
         audio_download = ctk.CTkButton(master=tab.tab("Audio"), text="Download", command=startDownloadAudio)
-        audio_download.pack(padx=10, pady=20)        
+        audio_download.pack(padx=10, pady=20)
+        
+        # Audio Current directory
+        audio_current_directory_label = ctk.CTkLabel(master=tab.tab("Audio"), text=f"Current Directory: {os.getcwd()}")
+        audio_current_directory_label.pack(padx=10, pady=10)
         
 if __name__ == "__main__":
     app = YT_Downloader()
